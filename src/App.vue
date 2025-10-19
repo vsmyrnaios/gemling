ead2f1a2966dd6fc6274a1d2e488d7fcf04c0057
vsmyrnaios <vasilissm52@gmail.com>
2025-10-19 18:34:37 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -128,12 +128,23 @@ function logout() {
 
 onMounted(() => {
   const savedPage = localStorage.getItem("lastPage");
+  const script = document.createElement("script");
 
   supabase.auth.getSession().then(({ data }) => {
     session.value = data.session;
     if (session.value) {
       currentPage.value = "account";
-    }
+    }    
+  script.src = "https://storage.ko-fi.com/cdn/scripts/overlay-widget.js";
+  script.onload = () => {
+    window.kofiWidgetOverlay.draw("gemmedtasks", {
+      type: "floating-chat",
+      "floating-chat.donateButton.text": "Support me",
+      "floating-chat.donateButton.background-color": "#00b9fe",
+      "floating-chat.donateButton.text-color": "#fff",
+    });
+  };
+  document.body.appendChild(script);
   });
 
   supabase.auth.onAuthStateChange((_, _session) => {
